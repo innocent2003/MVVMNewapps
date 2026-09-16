6260727aa02e49607f2de698ce2bccc3be791b35
innocent2003 <leminhtuanthpttranhungdaotx@gmail.com>
2026-09-16 23:16:51 +0700

    full pj

---
+++ app/src/main/java/com/example/myapplication/db/ArticleDao.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
---
+++ app/src/main/java/com/example/myapplication/ui/NewsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+
+class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+    }
+}
