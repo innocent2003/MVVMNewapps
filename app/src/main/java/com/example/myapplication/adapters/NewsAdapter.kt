8a26688c455c56b9120aecf5b546b277a05b5d7c
innocent2003 <leminhtuanthpttranhungdaotx@gmail.com>
2026-09-17 09:43:25 +0700

    update activity

--- app/src/main/java/com/example/myapplication/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapters/NewsAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.myapplication.R
 import com.example.myapplication.models.Article
 
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
